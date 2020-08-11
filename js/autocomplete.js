a4b34d33ff98eca31e0944e8d766706b9f41892e
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-08-11 14:07:43 -0700

    Created working autocomplete.

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,10 +1,47 @@
 const searchbar = document.getElementById('userField');
+const userList = document.querySelector('.user-list');
+const listItem = userList.children;
 const users = [
     "Victoria Chambers",
     "Dale Byrd",
     "Dawn Wood",
     "Dan Oliver"
 ];
+
+//Creates a list
+for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    let list = document.createElement('LI');
+    list.textContent = user;
+    userList.appendChild(list);
+    let listItem = userList.children[i];
+    listItem.className = "hidden";
+    console.log(listItem);
+}
+
+    //compares input to array
+searchbar.addEventListener('keyup', () => {
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i].toLowerCase();
+        if (user.includes(searchbar.value.toLowerCase()) && searchbar.value !== "") {
+            listItem[i].className = '';
+        } else {
+            listItem[i].className = 'hidden';
+        }
+    }
+});
+
+
+//Allows the user to click on one of the entries to add it to the searchbar value
+userList.addEventListener('click', (e) => {
+    let click = e.target.textContent;
+    for (let i = 0; i < users.length; i++) {
+        if (searchbar.value !== users[i]) {
+            searchbar.value = click;
+            userList.className = 'hidden';
+        }
+    }
+});
 
 // Create JS code to check the value of the searchbar, 
 // compare it to the values in the array, 
