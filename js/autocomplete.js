393314a71a6e61ed410e087249309c185ba458eb
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-08-11 14:21:15 -0700

    Fully functioning autocomplete

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,6 +1,6 @@
 const searchbar = document.getElementById('userField');
 const userList = document.querySelector('.user-list');
-const listItem = userList.children;
+const listItems = userList.children;
 const users = [
     "Victoria Chambers",
     "Dale Byrd",
@@ -16,17 +16,16 @@ for (let i = 0; i < users.length; i++) {
     userList.appendChild(list);
     let listItem = userList.children[i];
     listItem.className = "hidden";
-    console.log(listItem);
 }
 
-    //compares input to array
+//compares input to array
 searchbar.addEventListener('keyup', () => {
     for (let i = 0; i < users.length; i++) {
         let user = users[i].toLowerCase();
         if (user.includes(searchbar.value.toLowerCase()) && searchbar.value !== "") {
-            listItem[i].className = '';
+            listItems[i].className = '';
         } else {
-            listItem[i].className = 'hidden';
+            listItems[i].className = 'hidden';
         }
     }
 });
@@ -38,14 +37,8 @@ userList.addEventListener('click', (e) => {
     for (let i = 0; i < users.length; i++) {
         if (searchbar.value !== users[i]) {
             searchbar.value = click;
-            userList.className = 'hidden';
+            let listItem = userList.children[i];
+            listItem.className = "hidden"
         }
     }
 });
-
-// Create JS code to check the value of the searchbar, 
-// compare it to the values in the array, 
-// then append only those matching to the list div
-
-// Then figure out a way to allow it so that clicking on an entry 
-// in the drop down menu places that name into the searchbar.
