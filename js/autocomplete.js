d45ea9d7693c8b8406a8b06213e3db53be0acf60
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-08-10 17:57:23 -0700

    Started work on the autocomplete function

---
+++ js/autocomplete.js
@@ -0,0 +1,14 @@
+const searchbar = document.getElementById('userField');
+const users = [
+    "Victoria Chambers",
+    "Dale Byrd",
+    "Dawn Wood",
+    "Dan Oliver"
+];
+
+// Create JS code to check the value of the searchbar, 
+// compare it to the values in the array, 
+// then append only those matching to the list div
+
+// Then figure out a way to allow it so that clicking on an entry 
+// in the drop down menu places that name into the searchbar.
